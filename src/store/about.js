ae93bcb5e3c793e1c9b11c65b67d82ab76b3938d
Man-joke <akstns90@gmail.com>
2024-08-01 16:29:32 +0900

    about 전화번호 변경

--- src/store/about.js
+++ src/store/about.js
@@ -5,7 +5,7 @@ export default {
             name: "Man_Jock",
             email: 'akstns921@gmail.com',
             // blog: 'https://heropy.blog',
-            phone: '+82-01-1234-5678',
+            phone: '+82-01-8927-1926',
             image: 'https://avatars.githubusercontent.com/u/83157998?s=400&u=6af48d8da2857c44c14406cb2c10baeea66bc9df&v=4'
         }
     }
